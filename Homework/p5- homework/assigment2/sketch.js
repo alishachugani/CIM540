80248d424dec635a24f1b354ae206554d1473395
alisha chugani <ajc116@miami.edu>
2018-01-31 23:21:53 -0500

    arreglar nariz, hacerla mas grande y centrada

--- Homework/p5- homework/assigment2/sketch.js
+++ Homework/p5- homework/assigment2/sketch.js
@@ -9,20 +9,22 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  stroke(200)
+  stroke(255)
   strokeWeight(1)
   fill(255,204,0)
   ellipse(centerX+100,centerY,300,300)
 
+  noStroke
   fill(255)
   ellipse(centerX+50,centerY-50,50,50)
   ellipse(centerX+150,centerY-50,50,50)
 
+  noStroke
   fill(0)
   ellipse(centerX+50,centerY-45,20,20)
   ellipse(centerX+150,centerY-45,20,20)
 
-  noStroke
+  stroke(255,140,0)
   fill(255,140,0)
-  triangle(centerY+50, centerX+25, centerY+25, centerX-25, centerY+25)
+  triangle(centerX, centerY + 50, centerX + 25, centerY + 25, centerX - 25, centerY + 25);
 }
