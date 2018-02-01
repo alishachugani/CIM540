456f2a81c6a6fd26d2b1aef563e0f0692296f9ac
alisha chugani <ajc116@miami.edu>
2018-01-31 23:14:18 -0500

    cara pato

--- Homework/p5- homework/assigment2/sketch.js
+++ Homework/p5- homework/assigment2/sketch.js
@@ -9,4 +9,20 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  stroke(200)
+  strokeWeight(1)
+  fill(255,204,0)
+  ellipse(centerX+100,centerY,300,300)
+
+  fill(255)
+  ellipse(centerX+50,centerY-50,50,50)
+  ellipse(centerX+150,centerY-50,50,50)
+
+  fill(0)
+  ellipse(centerX+50,centerY-45,20,20)
+  ellipse(centerX+150,centerY-45,20,20)
+
+  noStroke
+  fill(255,140,0)
+  triangle(centerY+50, centerX + 25, centerY + 25, centerX - 25, centerY + 25)
 }
