9f3842f489592910d303a08a3b5b7e9594fbf6aa
alisha chugani <ajc116@miami.edu>
2018-02-01 12:43:51 -0500

    actually punto y coma

--- Homework/p5- homework/assigment2/sketch.js
+++ Homework/p5- homework/assigment2/sketch.js
@@ -9,22 +9,22 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  stroke(255)
-  strokeWeight(1)
-  fill(255,215,0)
-  ellipse(centerX+100,centerY,300,300)
+  stroke(255);
+  strokeWeight(1);
+  fill(255,215,0);
+  ellipse(centerX+100,centerY,300,300);
 
-  noStroke
-  fill(255)
-  ellipse(centerX+50,centerY-50,50,50)
-  ellipse(centerX+150,centerY-50,50,50)
+  noStroke;
+  fill(255);
+  ellipse(centerX+50,centerY-50,50,50);
+  ellipse(centerX+150,centerY-50,50,50);
 
-  noStroke
-  fill(0)
-  ellipse(centerX+50,centerY-45,20,20)
-  ellipse(centerX+150,centerY-45,20,20)
+  noStroke;
+  fill(0);
+  ellipse(centerX+50,centerY-45,20,20);
+  ellipse(centerX+150,centerY-45,20,20);
 
-  stroke(255,140,0)
-  fill(255,140,0)
+  stroke(255,140,0);
+  fill(255,140,0);
   triangle(centerX, centerY + 50, centerX + 25, centerY + 25, centerX - 25, centerY + 25);
 }
