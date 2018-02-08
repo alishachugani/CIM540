e177ee60ee94994c47c61dd4c2ce7d541b4aabf5
alisha chugani <ajc116@miami.edu>
2018-02-08 14:14:04 -0500

    botones (hay que corregir)

--- Homework/p5- homework/assigment2/sketch.js
+++ Homework/p5- homework/assigment2/sketch.js
@@ -1,30 +1,61 @@
 var centerX = 100;
 var centerY = 200;
+var button;
+var backgroundColor = "white"
+
+var sel;
+var lineColor = "red";
 
 function setup() {
   // put setup code here
   createCanvas(400,400);
   background(255);
+  button = createButton("Click Me");
+  button.mousePressed(changeBG);
+
+  sel = createSelect();
+  sel.option("blue");
+  sel.option("white");
+
+  sel.changed(function(){
+
+    lineColor = sel.value();
+  });
+
 }
 
 function draw() {
   // put drawing code here
-  stroke(255);
-  strokeWeight(1);
+  background(backgroundColor);
+
+  stroke(0);
+  strokeWeight(3);
   fill("yellow");
   ellipse(centerX+100,centerY,300,300);
 
-  noStroke;
+  stroke(0);
+  strokeWeight(3);
   fill(255);
   ellipse(centerX+50,centerY-50,50,50);
   ellipse(centerX+150,centerY-50,50,50);
 
-  noStroke;
   fill(0);
-  ellipse(centerX+50,centerY-45,20,20);
-  ellipse(centerX+150,centerY-45,20,20);
+  ellipse(centerX+50,centerY-40,25,25);
+  ellipse(centerX+150,centerY-40,25,25);
 
-  stroke(255,140,0);
-  fill(255,140,0);
-  triangle(centerX, centerY + 50, centerX + 25, centerY + 25, centerX - 25, centerY + 25);
+  noStroke();
+  fill(lineColor);
+  line(centerX + 50 ,centerY + 40, centerX + 150, centerY + 40);
+}
+
+function mousePressed(){
+  backgroundColor = "yellow";
+}
+
+function mouseReleased(){
+  backgroundColor = "red";
+}
+
+function changeBG(){
+  lineColor = "green";
 }
