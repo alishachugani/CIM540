8c0377d6b7de2ce1fe86f284e0ffb0c804c1ea2e
alisha chugani <ajc116@miami.edu>
2018-05-07 15:22:53 -0400

    wiejfw

--- Homework/p5- homework/assigment4/sketch.js
+++ Homework/p5- homework/assigment4/sketch.js
@@ -1,30 +1,25 @@
 
-var targetPosX = [0,200,400,600];
-
+var targetPosX = [0,200,400,600,800,1000,1200];
 var targetPosY = [0,200];
-
 var imageSize = 200;
-
 var pics = [];
-
 var outputText = "Find the soccer ball";
-
 var currentBox = 0;
-
 var startGame;
-
 var sg = false;
 
 function preload(){
   pics [0] = loadImage("assets/Football.png");
   pics [1] = loadImage("assets/basketball.png");
   pics [2] = loadImage("assets/soccer.png");
-
+  pics [3] = loadImage("assets/planet.jpg");
+  pics [4] = loadImage("assets/sun.jpg");
+  pics [5] = loadImage("assets/moon.jpg");
 
 }
 
   function setup (){
-    createCanvas(600,400);
+    createCanvas(1200,400);
     console.log("First position in targetPosx: " + targetPosX[0]);
     console.log("3rd position in targetPosx: " + targetPosX[2]);
     startGame = createButton("Click to start");
@@ -44,6 +39,9 @@ function preload(){
     rect(targetPosX[0], targetPosY[0], imageSize, imageSize);
     rect(targetPosX[1], targetPosY[0], imageSize, imageSize);
     rect(targetPosX[2], targetPosY[0], imageSize, imageSize);
+    rect(targetPosX[3], targetPosY[0], imageSize, imageSize);
+    rect(targetPosX[4], targetPosY[0], imageSize, imageSize);
+    rect(targetPosX[5], targetPosY[0], imageSize, imageSize);
 
     if(mouseX >= targetPosX[0] && mouseX < targetPosX[1] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
       console.log("Box 1");
@@ -63,6 +61,24 @@ function preload(){
       image(pics[2], targetPosX[2],targetPosY[0]);
       currentBox = 3;
     }
+
+    if(mouseX >= targetPosX[3] && mouseX < targetPosX[4] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 4");
+      image(pics[3], targetPosX[3],targetPosY[0]);
+      currentBox = 4;
+    }
+
+    if(mouseX >= targetPosX[4] && mouseX < targetPosX[5] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 5");
+      image(pics[4], targetPosX[4],targetPosY[0]);
+      currentBox = 5;
+    }
+
+    if(mouseX >= targetPosX[5] && mouseX < targetPosX[6] && mouseY >= targetPosY[0] && mouseY < targetPosY[1]){
+      console.log("Box 6");
+      image(pics[5], targetPosX[5],targetPosY[0]);
+      currentBox = 6;
+    }
   }
 textSize(32);
 text(outputText,50,250);
@@ -70,7 +86,7 @@ text(outputText,50,250);
 if(currentBox == 3){
   console.log("You win!!");
   outputText = "You win!!"
-}else if(currentBox == 1 || currentBox == 2){
+}else if(currentBox == 1 || currentBox == 2 || currentBox == 4 || currentBox == 5 || currentBox == 6){
   console.log("You lose");
   outputText = "You lose"
 }else{
